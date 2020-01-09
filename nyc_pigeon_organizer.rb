a5391ddffa16ca3a68e967a7a295b3614c1afff3
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-09 17:33:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   pigeon_name_list = {}
   
-  data.each do |hash_1, key_1|
+data.each do |hash_1, key_1|
   key_1.each do |hash_2, key_2|
     key_2.each do |name|
       pigeon_name_list[name] = {:color => [], :gender => [], :lives => []}
@@ -10,25 +10,25 @@ def nyc_pigeon_organizer(data)
 end
 
 data[:color].each do |hash_1, key_1|
-  key_1.each do |pigeon_color|
-    if data[:color][hash_1].include? pigeon_color
-    pigeon_name_list[pigeon_color][:color] << hash_1.to_s
+  key_1.each do |name|
+    if data[:color][hash_1].include? name
+    pigeon_name_list[name][:color] << hash_1.to_s
     end
   end
 end
 
 data[:gender].each do |hash_1, key_1|
-  key_1.each do |pigeon_gender|
-    if data[:gender][hash_1].include? pigeon_gender
-    pigeon_name_list[pigeon_gender][:gender] << hash_1.to_s
+  key_1.each do |name|
+    if data[:gender][hash_1].include? name
+    pigeon_name_list[name][:gender] << hash_1.to_s
     end
   end
 end
 
 data[:lives].each do |hash_1, key_1|
-  key_1.each do |pigeon_lives|
-    if data[:lives][hash_1].include? pigeon_lives
-    pigeon_name_list[pigeon_lives][:lives] << hash_1.to_s
+  key_1.each do |name|
+    if data[:lives][hash_1].include? name
+    pigeon_name_list[name][:lives] << hash_1.to_s
     end
   end
 end
