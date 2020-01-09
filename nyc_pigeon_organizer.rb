5a1b19a1ccccd8cb2f95c0a747ab9aefd2789570
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-09 17:39:56 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,38 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_name_list = {}
+  
+data.each do |hash_1, key_1|
+  key_1.each do |hash_2, key_2|
+    key_2.each do |name|
+      pigeon_name_list[name] = {:color => [], :gender => [], :lives => []}
+    end
+  end
+end
+
+data[:color].each do |hash_1, key_1|
+  key_1.each do |name|
+    if data[:color][hash_1].include? name
+    pigeon_name_list[name][:color] << hash_1.to_s
+    end
+  end
+end
+
+data[:gender].each do |hash_1, key_1|
+  key_1.each do |name|
+    if data[:gender][hash_1].include? name
+    pigeon_name_list[name][:gender] << hash_1.to_s
+    end
+  end
+end
+
+data[:lives].each do |hash_1, key_1|
+  key_1.each do |name|
+    if data[:lives][hash_1].include? name
+    pigeon_name_list[name][:lives] << hash_1.to_s
+    end
+  end
+end
+
+pigeon_name_list
+
 end
